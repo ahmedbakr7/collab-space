4e4d6a69e3158a397c5dbbb2e693c0106f479b59
ahmedbakr7 <ahmad5baker2002@gmail.com>
2026-02-08 16:05:11 +0200

    feat: successfully integrated Swagger (OpenAPI) into the NestJS API.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppErrorFilter } from './shared/filters/app-error.filter';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Collab Space API')
+    .setDescription('The Collab Space API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   app.useGlobalFilters(new AppErrorFilter());
   app.enableCors();
   await app.listen(3000);
